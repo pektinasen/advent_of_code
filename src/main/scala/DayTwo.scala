c2cdcdc5e8eed10aa2e5afe298c918e9b7aa9a62
Saskia Gennrich <saskia@gennri.ch>
2017-12-03 21:58:14 +0100

    for some reason this is not compiling

---
+++ src/main/scala/DayTwo.scala
@@ -0,0 +1,29 @@
+object DayTwo {
+
+  def folding(z: Int, mm: (Int, Int)): (Int, Int) = {
+    val (min, max) = mm
+    z match {
+      case x if x < min && x > max => (x, x)
+      case x if x < min && x < max => (x, max)
+      case x if x > min && x > max => (min, x)
+      case _ => (min, max)
+    }
+  }
+
+  def minMax(list: List[Int]): (Int, Int) =
+    list.fold((Int.MaxValue, Int.MinValue))((x:Int, mm:(Int, Int)) => folding(x,mm))
+
+  def solve(sheet: List[List[Int]]): Int = {
+    ???
+  }
+
+  
+  def main(args: Array[String]): Unit = {
+    val data = scala.io.Source.fromFile("src/main/scala/day_two_input.txt")
+      .mkString
+      .split("\\n").toList
+      .map { _.split("\\t").toList.map {_.toInt}}
+    val result = solve(data)
+    println(result)
+  }
+}
